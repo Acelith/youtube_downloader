b41d223357f0caefb4f404aa0afcf997c8f66c2e
Joël <joel.jon@moix.me>
2021-05-21 16:26:00 +0200

    Aggiunte classi
    Aggiunta classe per scaricare playlist
    Aggiunta classe per creare directory
    Aggiunta classe per scelta directroy

--- functions.py
+++ functions.py
@@ -1,18 +1,63 @@
 from tkinter.filedialog import askdirectory
 from pytube import YouTube
+from pytube import Playlist
 from tkinter import * 
 import os
-def scaricaVideo(url):
+def scaricaVideo(url, directory):
     try:
         video = YouTube(url)
         video = video.streams.first()
-        directory = askdirectory()
-        
-        if directory == '':
-            directory = os.getcwd()
-
+    
         video.download(directory)
 
         return "Il video è stato scaricato correttamente "
-    except Exception as e:
+    except Exception:
         return "Non è stato possibile scaricare il file, riprovare piu tardi"
+
+
+def scaricaPlaylist(url):
+    try: 
+        directory = askDirectory()
+        playlist = Playlist(url)
+        urls = playlist.video_urls
+        directory = createDirectory(directory, playlist.title)
+        for link in urls:
+            scaricaVideo(link, directory)
+
+        return "La playlist è stata scaricata correttamente nella cartella: " + directory
+    except Exception:
+        return "Non è stato possibile scaricare la playlist, riprovare piu tardi"
+
+def createDirectory(path, name):
+    try:
+        finalDirectory = 0
+        if os.path.exists(path + "/" + name):
+            i = 0
+            digit = 1
+            while i < 1:
+                if os.path.isdir(path + "/" + name + " - " + str(digit)):
+                    digit = digit + 1
+                else:
+                    os.makedirs(path + "/" + name + " - " + str(digit))
+                    finalDirectory  = path + "/" + name + " - " + str(digit)
+                    break
+
+        else:
+            #os.mkdir(path)
+            os.makedirs(path + "/" + name)
+            finalDirectory  = path + "/" + name
+
+        return finalDirectory
+    except Exception:
+        return "Errore"
+
+
+def askDirectory():
+    directory = askdirectory()
+        
+    if directory == '':
+        directory = os.getcwd()
+    return directory
+
+print(scaricaPlaylist("https://youtube.com/playlist?list=PLHLua7lnY9X-uAKqwp0T23h3A4d-ZajTO"))
+
--- guy.py
+++ guy.py
@@ -1,17 +1,31 @@
 import tkinter as tk
 #import functions.py 
 
+def scaricaVideo():
+    return ""
+
 window = tk.Tk()
 window.title("Scarica Video")
+
+tk.Label(window, text="First Name").grid(row=0)
+tk.Label(window, text="Last Name").grid(row=1)
+
+e1 = tk.Entry(window)
+e2 = tk.Entry(window)
 
 scarica = tk.Button(
     text="Scarica video",
     width=25,
     height=5,
-    fg="red",
+    bg="red",
+    command=scaricaVideo(),
 )
-scarica.pack()
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+scarica.grid(row=2, column=1)
+
+#scarica.pack()
+
 window.mainloop()
-
-
 
