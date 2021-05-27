0de5efa136cc2eb01c22d9c213125275e7b7d938
Acelith <64836338+Acelith@users.noreply.github.com>
2021-05-27 19:34:22 +0200

    cambiamento gui

--- guy.py
+++ guy.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from tkinter.filedialog import askdirectory
 from pytube import YouTube
 from pytube import Playlist
-from tkinter import * 
+import tkinter as tk
 import os
 
 playlist_lengt = 0
@@ -20,27 +20,27 @@ def scaricaVideo(url, directory):
         #video.register_on_complete_callback(window['PROGRESS'].update('Ho finito di scaricare ora puoi guardare il video ' + video.title()))
 
         video.download(directory)
-        
+
         return 1
     except Exception:
         return 2
 """
 @Name: scaricaPlaylist
 @desc: si occupa di scaricare i video presenti nella playlist inviata
-@parameters: 
+@parameters:
     url{String}: URL della playlist da scaricare
 
 """
 
 def scaricaPlaylist(url, directory):
-    try: 
-        #directory = askDirectory()
+    try:
         playlist = Playlist(url)
         video_list = playlist.video_urls
         directory = createDirectory(directory, playlist.title)
 
         #Scarica i video e li inserisce nella directory
         prog = 0
+
         playlist_lengt = playlist.length
         for link in video_list:
             res = scaricaVideo(link, directory)
@@ -51,7 +51,7 @@ def scaricaPlaylist(url, directory):
                 prog = prog + res
 
             window['PROGRESS'].update_bar(prog)
-
+       
     except Exception:
         return "Errore: la playlist non è stata scaricata correttamente"
 
@@ -108,43 +108,16 @@ def askDirectory():
     return directory
 
 
-# Define the window's contents
-layout = [[sg.Text("Url")],
-          [sg.Input(key='-URL-')], [sg.Image(size=(100,100),filename='logo.gif')],
-          [sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Text("Percorso di download"), sg.Input(key='-PERCORSO-'), sg.Button('...') ],
-          [sg.Button('Scarica'), sg.Button('Esci'), sg.ProgressBar(max_value=playlist_lengt,size=(40,20), key='PROGRESS')]]
-#[[sg.Radio('Video', 'choose', key='c_video', default=True) , sg.Radio('Playlist', 'choose' , key='c_playlist')]],
+# gui
 
-# Create the window
-window = sg.Window('Scarica video e playlist da youtube', layout)
+finestra = tk.Tk()
+#widget
+lbl_download_video = tk.Label(text="Inserisci url da scaricare", width=50, height=3)
+url_field = tk.Entry(finestra,width=50)
+download_btn = tk.Button(text="Avvia download",  width=10,height=4,command=scaricaPlaylist)
 
-# Display and interact with the Window using an Event Loop
-while True:
-    event, values = window.read()
-    url = values['-URL-']
-    # See if user wants to quit or window was closed!
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
-
-    elif event == '...':
-        cartella = askDirectory()
-        window['-PERCORSO-'].update(cartella)
-    #Scarica media
-    elif event == 'Scarica':
-        try:
-            if "playlist" in url:
-                scaricaPlaylist(url, values['-PERCORSO-'])
-                window['-OUTPUT-'].update('la tua playlist sta venendo scaricata, per favore aspetta')
-            else:
-                scaricaVideo(url, values['-PERCORSO-'])
-            window['-OUTPUT-'].update('il tuo video sta venendo scaricato, per favore aspetta')
-        except Exception:
-            window['-OUTPUT-'].update('Non funziona niente cribbio')
-    
-   
-
-# Finish up by removing from the screen
-window.close()
-
-#TODO Controllare i radio button se selezionata playlist oppure video, ultima cosa in cantiere, il progetto e da finire
+#pack
+lbl_download_video.pack()
+url_field.pack()
+download_btn.pack()
+finestra.mainloop()
